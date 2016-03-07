19d9111ec743d0ce288934cfd3499878ed86f688
Amitaibu <amitai@gizra.com>
2016-03-07 16:14:43 +0200

    Rename function

--- src/elm/PersonTest.elm
+++ src/elm/PersonTest.elm
@@ -8,26 +8,26 @@ decrementActionSuite : Test
 decrementActionSuite =
   suite "Decerment action"
     -- Kids = -1 -> Kids = 0
-    [ test "negative count" (assertEqual 0 (updateCounter Person.Decrement -1))
+    [ test "negative count" (assertEqual 0 (updateKids Person.Decrement -1))
     -- Kids = 0 -> Kids = 0
-    , test "zero count" (assertEqual 0 (updateCounter Person.Decrement 0))
+    , test "zero count" (assertEqual 0 (updateKids Person.Decrement 0))
     -- Kids = 1 -> Kids = 0
-    , test "positive count" (assertEqual 0 (updateCounter Person.Decrement 1))
+    , test "positive count" (assertEqual 0 (updateKids Person.Decrement 1))
     -- Kids = 5 -> Kids = 4
-    , test "top limit count" (assertEqual 4 (updateCounter Person.Decrement 5))
+    , test "top limit count" (assertEqual 4 (updateKids Person.Decrement 5))
     ]
 
 incrementActionSuite : Test
 incrementActionSuite =
   suite "Incerment action"
-    [ test "negative count" (assertEqual 0 (updateCounter Person.Increment -1))
-    , test "zero count" (assertEqual 1 (updateCounter Person.Increment 0))
-    , test "positive count" (assertEqual 2 (updateCounter Person.Increment 1))
-    , test "top limit count" (assertEqual 5 (updateCounter Person.Increment 5))
+    [ test "negative count" (assertEqual 0 (updateKids Person.Increment -1))
+    , test "zero count" (assertEqual 1 (updateKids Person.Increment 0))
+    , test "positive count" (assertEqual 2 (updateKids Person.Increment 1))
+    , test "top limit count" (assertEqual 5 (updateKids Person.Increment 5))
     ]
 
-updateCounter : Person.Action -> Int -> Int
-updateCounter action initialKids =
+updateKids : Person.Action -> Int -> Int
+updateKids action initialKids =
   let
     model = { initialModel | kids = (Person.Kids initialKids) }
     model' = fst <| Person.update action model
